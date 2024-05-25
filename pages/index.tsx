869ba2cc20b6e45011c55d839fd042397919aa67
Rohit Singh <rs9990271.edu@gmail.com>
2024-05-25 19:46:49 +0530

    cleared clutter

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Logo from "@/components/Logo";
 import Features from "@/components/Features";
 import Why from "@/components/Why";
 import WhoUses from "@/components/WhoUses";
-import Footer2 from "@/components/Footer2";
 import { IndiaFirst } from "@/components/IndiaFirst";
 import { FAQ } from "@/components/FAQ";
 import Header2 from "@/components/Header2";
