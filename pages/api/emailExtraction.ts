49c9c61e95197f796298afcda31d0f98ed2b0659
Rohit Singh <rs9990271.edu@gmail.com>
2024-06-30 10:07:08 +0530

    final set of changes

--- pages/api/emailExtraction.ts
+++ pages/api/emailExtraction.ts
@@ -1,17 +1,71 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
 import cheerio from "cheerio";
+import puppeteer from "puppeteer";
+import fs from "fs";
+import path from "path";
 
-async function findEmailAddresses(url: string): Promise<string[]> {
+var web_brower = null;
+async function web_tab(url: string, browser) {
+  const start = new Date().getTime();
+  const page = await browser.newPage();
   try {
-    const response = await axios.get(url, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-      },
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      const resource = request.resourceType();
+      if (
+        resource === "image" ||
+        resource === "media" ||
+        resource === "font" ||
+        resource === "stylesheet"
+      ) {
+        request.abort();
+      } else {
+        request.continue();
+      }
     });
-    const html = response.data;
-    const $ = cheerio.load(html);
+
+    await page.goto(url, { waitUntil: "domcontentloaded", timeout: 10000 });
+    const html = await page.content();
+    const end = new Date().getTime();
+    await writeToFile(
+      "web_driver processed page: " + url + " time " + (end - start) / 60000.0
+    );
+    return html;
+  } catch (error) {
+  } finally {
+    await page.close();
+  }
+}
+async function web_driver() {
+  let browser;
+  try {
+    //  this launch new instance browser \
+    // { waitUntil: 'networkidle0' }
+    browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox"],
+      devtools: true,
+    });
+    return browser;
+  } catch (error) {
+    console.error(error);
+  }
+
+  return "";
+}
+
+async function findEmailAddresses($, url): Promise<string[]> {
+  try {
+    // const response = await axios.get(url, {
+    //   headers: {
+    //     "User-Agent":
+    //       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    //   },
+    // });
+    // const html = response.data;
+    //  const demo = await  web_tab(url,web_brower);
+    //  const start = new Date().getTime();
+    //   const $ = cheerio.load( demo);
     const emailAddresses: string[] = [];
 
     $("a[href], p, span, li, td").each((index, element) => {
@@ -65,21 +119,45 @@ async function findEmailAddresses(url: string): Promise<string[]> {
   }
 }
 
+async function writeToFile(text: string) {
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split("T")[0]; // YYYY-MM-DD format
+
+  const filePath = path.join(process.cwd(), "logs", `log_${formattedDate}.txt`);
+
+  const dir = path.dirname(filePath);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+
+  const currentTime = new Date().toISOString();
+  const logEntry = `[${currentTime}] : ${text}\n`;
+  fs.appendFileSync(filePath, logEntry, "utf8");
+}
+
 async function crawlWebsite(startUrls: string[]) {
   const allWebsitesData: object[] = [];
 
   for (const startUrl of startUrls) {
     const visited = new Set<string>();
+
     const queue: string[] = [startUrl];
 
     while (queue.length > 0) {
       const currentUrl = queue.shift();
+
       if (!currentUrl || visited.has(currentUrl)) {
         continue;
       }
+
       visited.add(currentUrl);
+
       try {
-        const emailAddresses = await findEmailAddresses(currentUrl);
+        const demo = await web_tab(currentUrl, web_brower);
+        if (demo === "") continue;
+        const start = new Date().getTime();
+        const $ = cheerio.load(demo);
+        const emailAddresses = await findEmailAddresses($, currentUrl);
 
         if (emailAddresses.length > 0) {
           const websiteData = {
@@ -95,14 +173,14 @@ async function crawlWebsite(startUrls: string[]) {
           break; // Stop crawling this website after finding emails
         }
 
-        const response = await axios.get(currentUrl, {
-          headers: {
-            "User-Agent":
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-          },
-        });
-        const html = response.data;
-        const $ = cheerio.load(html);
+        // const response = await axios.get(currentUrl, {
+        //   headers: {
+        //     "User-Agent":
+        //       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        //   },
+        // });
+        // const html = response.data;
+        // const $ = cheerio.load(html);
         const links = $("a[href]");
         links.each((index, element) => {
           const absoluteUrl = new URL($(element).attr("href")!, currentUrl)
@@ -110,8 +188,16 @@ async function crawlWebsite(startUrls: string[]) {
           queue.push(absoluteUrl);
         });
 
+        const end = new Date().getTime();
+
+        await writeToFile(
+          "cheerio processed page: " +
+            currentUrl +
+            " time " +
+            (end - start) / 60000.0
+        );
         // Add delay to avoid rate limiting
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+        // await new Promise((resolve) => setTimeout(resolve, 1000));
       } catch (error) {
         console.error(`Error while processing ${currentUrl}: ${error}`);
       }
@@ -138,10 +224,22 @@ export default async function handler(
   }
 
   try {
+    const start = new Date().getTime();
+    web_brower = await web_driver();
     const allWebsitesData = await crawlWebsite(startingUrls);
     res.status(200).json({ websites: allWebsitesData });
+    const end = new Date().getTime();
+
+    await writeToFile(
+      "process complete for  page: " +
+        startingUrls +
+        " time " +
+        (end - start) / 60000.0
+    );
   } catch (error) {
     console.error(`Error while crawling websites: ${error}`);
     res.status(500).json({ message: "Internal Server Error" });
+  } finally {
+    web_brower.close();
   }
 }
